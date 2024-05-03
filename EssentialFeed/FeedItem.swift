d2b03e53795aca9a6f8d91a68a847b5f141e4430
Andrea Miotto <miotto.andrea@icloud.com>
2024-05-03 14:37:13 +0100

    Add FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Andrea on 03/05/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
