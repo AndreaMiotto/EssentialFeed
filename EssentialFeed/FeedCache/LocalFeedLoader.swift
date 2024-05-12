b47bd7252d9eb3fd07ba3d7e3dd9660d67284883
Andrea Miotto <miotto.andrea@icloud.com>
2024-05-12 15:09:21 +0100

    Load command delivers no images on seven days old cache

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -39,15 +39,24 @@ public final class LocalFeedLoader {
     }
 
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { result in
+        store.retrieve { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-            case let .found(feed, _):
+            case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
-            case .empty:
+            case .found, .empty:
                 completion(.success([]))            }
         }
+    }
+
+    private func validate(_ timestamp: Date) -> Bool {
+        let calendar = Calendar(identifier: .gregorian)
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
     }
 }
 
@@ -58,7 +67,7 @@ private extension Array where Element == FeedImage {
 }
 
 private extension Array where Element == LocalFeedImage {
-     func toModels() -> [FeedImage] {
-         return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-     }
- }
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
